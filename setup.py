0d52fd1c3d2eca9a387200929ba7d87636e7f0d4
PXD <79040730+PXD-mc@users.noreply.github.com>
2022-10-13 22:23:44 +0500

    update from beta-0.1b to beta-0.1c

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 import setuptools
-setuptools.setup(name='Nethergames.py',version='beta-0.1b',description='Allows you to interact with Nethergames API. This module is not affiliated with Nethergames.',author='PXD',packages=['nethergames'])
+setuptools.setup(name='Nethergames.py',version='beta-0.1c',description='Allows you to interact with Nethergames API. This module is not affiliated with Nethergames.',author='PXD',packages=['nethergames'])
