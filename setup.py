f7d464bb77d1a72ed0dd94fd1dfdb0094aab4206
PXD <79040730+PXD-mc@users.noreply.github.com>
2022-10-19 15:07:55 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 import setuptools
-setuptools.setup(name='NetherGames',version='0+1',description='Allows you to interact with Nethergames API. This module is not affiliated with Nethergames.',author='PXD',packages=['nethergames'])
+setuptools.setup(name='NetherGames',version='0-1bc',description='Allows you to interact with Nethergames API. This module is not affiliated with Nethergames.',author='PXD',packages=['nethergames'])
