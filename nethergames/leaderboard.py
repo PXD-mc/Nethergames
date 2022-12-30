776cd2897f21d22a11aec6026c9228f64d6e3c7c
PXD <79040730+PXD-mc@users.noreply.github.com>
2022-12-30 19:14:38 +0500

    Update leaderboard.py

--- nethergames/leaderboard.py
+++ nethergames/leaderboard.py
@@ -1,13 +1,13 @@
 import urllib.request,json
 class Leaderboard():
   def __init__(self,type:str,subtype:str=None,limit:int=100):
-    self.type = type
+    self.type = str(type)
     if (self.type).lower() == 'game':
       if subtype == None:
         raise Exception("\n\nsubtype cannot be empty when type is 'game'\nLeaderboard(type,subtype,limit)")
       request = urllib.request.Request(f"https://apiv2.nethergames.org/v1/leaderboard?type=game&column={subtype}&limit={limit}", headers={'User-Agent': 'Mozilla/91.0'})
     else:
-      request = urllib.request.Request(f"https://apiv2.nethergames.org/v1/leaderboard?type={type}&limit={limit}", headers={'User-Agent': 'Mozilla/91.0'})
+      request = urllib.request.Request(f"https://apiv2.nethergames.org/v1/leaderboard?type={str(type)}&limit={limit}", headers={'User-Agent': 'Mozilla/91.0'})
     response = urllib.request.urlopen(request)
     self.data = json.loads(response.read().decode('UTF-8'))
     
